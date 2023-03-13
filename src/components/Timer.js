eaf3c8f5d9b09b422006dc0171c6a024dab270ef
Vansh Baghel <vanshbaghel912@gmail.com>
2023-03-13 23:45:28 +0530

    Timer design change

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,7 +5,7 @@ const Timer = () => {
   return (
     <div>
       <div className="text-center">
-        <Countdown date={Date.now() + 100000} className='text-3xl'/>
+        <Countdown date={Date.now() + 100000} className='text-2xl bg-white text-black p-4 rounded-lg'/>
       </div>
     </div>
   );

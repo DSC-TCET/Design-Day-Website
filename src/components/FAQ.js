816793701e933d7bfd7bc3d1fb65c92cde8ebc32
Vansh Baghel <vanshbaghel912@gmail.com>
2023-03-14 23:23:13 +0530

    FAQ fixed

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -16,7 +16,7 @@ export default function SimpleAccordion() {
           FAQ
         </h2>
         <Accordion
-          className="m-10 drop-shadow-xl"
+          className="m-10 drop-shadow-xl "
           style={{
             margin: isMobile ? "2rem" : null,
           }}
@@ -41,18 +41,19 @@ export default function SimpleAccordion() {
             }}
           >
             <Typography>
-              Design Day is an event where participants of Google Solution Challenge present their prototypes they have developed.
+              Design Day is an event where participants of Google Solution
+              Challenge present their prototypes they have developed.
             </Typography>
           </AccordionDetails>
         </Accordion>
         <Accordion
-          className="m-10"
+          className="m-10 drop-shadow-xl "
           style={{
             margin: isMobile ? "2rem" : null,
           }}
         >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             aria-controls="panel2a-content"
             id="panel2a-header"
             sx={{
@@ -60,19 +61,31 @@ export default function SimpleAccordion() {
               color: "rgba(252, 244, 244, 0.768)",
             }}
           >
-            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Do we need to submit our project in advance?</Typography>
+            <Typography sx={{ fontSize: "2rem", margin: ".5rem" }}>
+              Do we need to submit our project in advance?
+            </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
             <Typography>
-              Yes, teams are required to submit their projects at least 24 hours before the event.
+              Yes, teams are required to submit their projects at least 24 hours
+              before the event.
             </Typography>
           </AccordionDetails>
-          <AccordionSummary />
         </Accordion>
 
-        <Accordion className='m-10'>
+        <Accordion
+          className="m-10 drop-shadow-xl "
+          style={{
+            margin: isMobile ? "2rem" : null,
+          }}
+        >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             aria-controls="panel2a-content"
             id="panel2a-header"
             sx={{
@@ -80,18 +93,30 @@ export default function SimpleAccordion() {
               color: "rgba(252, 244, 244, 0.768)",
             }}
           >
-            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>How do I register my team for Design Day?</Typography>
+            <Typography sx={{ fontSize: "2rem", margin: ".5rem" }}>
+              How do I register my team for Design Day?
+            </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
             <Typography>
               Register your team on the above register button on this website.
             </Typography>
           </AccordionDetails>
         </Accordion>
 
-        <Accordion className='m-10'>
+        <Accordion
+          className="m-10 drop-shadow-xl "
+          style={{
+            margin: isMobile ? "2rem" : null,
+          }}
+        >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             aria-controls="panel2a-content"
             id="panel2a-header"
             sx={{
@@ -99,18 +124,31 @@ export default function SimpleAccordion() {
               color: "rgba(252, 244, 244, 0.768)",
             }}
           >
-            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Do we need to submit our project in advance?</Typography>
+            <Typography sx={{ fontSize: "2rem", margin: ".5rem" }}>
+              Do we need to submit our project in advance?
+            </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
             <Typography>
-              Yes, teams are required to submit their projects at least 24 hours before the event.
+              Yes, teams are required to submit their projects at least 24 hours
+              before the event.
             </Typography>
           </AccordionDetails>
         </Accordion>
 
-        <Accordion className='m-10'>
+        <Accordion
+          className="m-10 drop-shadow-xl "
+          style={{
+            margin: isMobile ? "2rem" : null,
+          }}
+        >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             aria-controls="panel2a-content"
             id="panel2a-header"
             sx={{
@@ -118,18 +156,31 @@ export default function SimpleAccordion() {
               color: "rgba(252, 244, 244, 0.768)",
             }}
           >
-            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>What should we bring to the event?</Typography>
+            <Typography sx={{ fontSize: "2rem", margin: ".5rem" }}>
+              What should we bring to the event?
+            </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
             <Typography>
-              Teams must bring their own equipment, including laptops, chargers, and any other necessary tools.
+              Teams must bring their own equipment, including laptops, chargers,
+              and any other necessary tools.
             </Typography>
           </AccordionDetails>
         </Accordion>
 
-        <Accordion className='m-10'>
+        <Accordion
+          className="m-10 drop-shadow-xl "
+          style={{
+            margin: isMobile ? "2rem" : null,
+          }}
+        >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             aria-controls="panel2a-content"
             id="panel2a-header"
             sx={{
@@ -137,18 +188,30 @@ export default function SimpleAccordion() {
               color: "rgba(252, 244, 244, 0.768)",
             }}
           >
-            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Will there be judges?</Typography>
+            <Typography sx={{ fontSize: "2rem", margin: ".5rem" }}>
+              Will there be judges?
+            </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
             <Typography>
               No, mentors will be present instead of judges.
             </Typography>
           </AccordionDetails>
         </Accordion>
 
-        <Accordion className='m-10'>
+        <Accordion
+          className="m-10 drop-shadow-xl "
+          style={{
+            margin: isMobile ? "2rem" : null,
+          }}
+        >
           <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
+            expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
             aria-controls="panel2a-content"
             id="panel2a-header"
             sx={{
@@ -156,18 +219,23 @@ export default function SimpleAccordion() {
               color: "rgba(252, 244, 244, 0.768)",
             }}
           >
-            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Can we exceed our presentation time?</Typography>
+            <Typography sx={{ fontSize: "2rem", margin: ".5rem" }}>
+              Can we exceed our presentation time?
+            </Typography>
           </AccordionSummary>
-          <AccordionDetails>
+          <AccordionDetails
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
             <Typography>
-              No, any team that exceeds the allotted time will be asked to conclude their presentation.
+              No, any team that exceeds the allotted time will be asked to
+              conclude their presentation.
             </Typography>
           </AccordionDetails>
         </Accordion>
-
       </div>
-     
-    </section >
-    
+    </section>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,15 +8,32 @@ const Home = () => {
 
   return (
     <section id="home" className="mt-16">
-      <div style={{display : isMobile ? 'grid' : 'flex'}}>
-        <div style={{width : isMobile ? '100%' : '50%'}}>
-          <div className="ml-10 flex justify-center items-center h-full capitalize" style={{fontSize : isMobile ? '2rem' : '4rem', marginTop: isMobile ? '2rem' : null}}>design day</div>
+      <div style={{ display: isMobile ? "grid" : "flex" }}>
+        <div style={{ width: isMobile ? "100%" : "50%" }}>
+          <div
+            className="ml-10 flex justify-center items-center h-full capitalize"
+            style={{
+              fontSize: isMobile ? "2rem" : "4rem",
+              marginTop: isMobile ? "2rem" : null,
+            }}
+          >
+            design day
+          </div>
         </div>
-        <div style={{width : isMobile ? '100%' : '50%', margin: isMobile ? '5rem 2rem 0 .5rem' : null}}> 
+        <div
+          style={{
+            width: isMobile ? "100%" : "50%",
+            margin: isMobile ? "5rem 2rem 0 .5rem" : null,
+          }}
+        >
           <img src={img} alt="img" />
         </div>
-          <div class="sub">
-          <a href="https://google.com"><button class="reg">Register</button><Timer /></a></div>
+      </div>
+      <div class="sub">
+        <a href="https://google.com">
+          <button class="reg">Register</button>
+          <Timer />
+        </a>
       </div>
     </section>
   );

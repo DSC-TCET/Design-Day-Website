1060ffeb7520435936ade6c67cbd7c07096f543a
Sanskar Mishra <95489547+whythoSanskar@users.noreply.github.com>
2023-03-14 21:35:10 +0530

    Branch3 (#7)
    * initial commit (#2)
    
    Co-authored-by: Sanskar <mishrasanskar@gmail.com>
    
    * Replaced with react
    
    * Improved the UI, kept it basic though
    
    * removed node modules
    
    * Improved FAQ
    
    * removing node_modules
    
    * Color changed
    
    * Added countdown, integrated few navbar
    
    * Timer design change
    
    * Content written
    
    * resolving merge conflict
    
    * resolving conflicts
    
    * replaced location of timer
    
    * aligning timer
    
    * modified a file
    
    ---------
    
    Co-authored-by: Prathik Shetty <63772910+prathikshetty2002@users.noreply.github.com>
    Co-authored-by: Sanskar <mishrasanskar@gmail.com>
    Co-authored-by: Vansh Baghel <vanshbaghel912@gmail.com>

---
+++ src/components/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="my-20">
+      <div className="mt-20" id="about">
+        <h2 className="m-6 text-5xl text-center">About</h2>
+        <div className="mx-16 mb-10 text-xl tracking-wider">
+          Design Day is a program organized by Google as part of its Solution
+          Challenge initiative. The Solution Challenge is an annual competition
+          that encourages students and young professionals to use technology to
+          solve real-world problems. <br></br><br></br>The competition is ogtipen to individuals and
+          teams of up to four members, and participants are required to submit a
+          solution that addresses one of Google's designated global problems.
+          Design Day is a one-day event that takes place as part of the Solution
+          Challenge. During the event, participants are invited to collaborate
+          with each other and with Google mentors to develop their proposed
+          solutions.<br></br><br></br>  The event provides an opportunity for participants to
+          refine their ideas, receive feedback from experts, and learn more
+          about Google's technologies and design principles.<br></br><br></br> Design Day
+          typically involves a series of workshops and activities focused on
+          design thinking and problem-solving. Participants have the opportunity
+          to work with Google's Design Sprint methodology, which involves a
+          five-day process for solving complex problems and creating new
+          products or services.<br></br><br></br> At the end of Design Day, participants present
+          their solutions to a panel of judges, who evaluate the proposals based
+          on criteria such as innovation, impact, and feasibility. The winning
+          teams receive prizes and may also have the opportunity to work with
+          Google to further develop and implement their solutions. Overall,
+          Design Day is a valuable opportunity for young professionals to gain
+          experience in design thinking and problem-solving, collaborate with
+          others, and work on solutions to real-world problems.
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/FAQ.js
@@ -0,0 +1,152 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function SimpleAccordion() {
+  return (
+    <section className="my-20" className="my-20">
+      <div id="faq">
+        <h2 className="m-6 text-5xl ">FAQ</h2>
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>What is Design Day?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Design Day is an event where participants of Google Solution Challenge present their prototypes they have developed.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Do we need to submit our project in advance?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Yes, teams are required to submit their projects at least 24 hours before the event.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>How do I register my team for Design Day?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Register your team on the above register button on this website.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Do we need to submit our project in advance?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Yes, teams are required to submit their projects at least 24 hours before the event.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>What should we bring to the event?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Teams must bring their own equipment, including laptops, chargers, and any other necessary tools.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Will there be judges?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              No, mentors will be present instead of judges.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion className='m-10'>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+            sx={{
+              backgroundColor: "#1b1e29",
+              color: "rgba(252, 244, 244, 0.768)",
+            }}
+          >
+            <Typography sx={{ fontSize: '2rem', margin: '.5rem' }}>Can we exceed our presentation time?</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              No, any team that exceeds the allotted time will be asked to conclude their presentation.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+
+      </div>
+     
+    </section >
+    
+      </div>
+    </section>
+  );
+}
